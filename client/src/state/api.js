e6b9fcdf2c12ca9afae451b999d1b6767551288d
Parvchoudhary <parv.crn@gmail.com>
2023-07-14 22:38:43 +0530

    Initialising repo

---
+++ client/src/state/api.js
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: ["User", "Products", "Customers","Transactions","Geography","Sales"],
+
+  endpoints: (build) => ({
+    getUser: build.query({
+      query: (id) => `/general/user/${id}`,
+      providesTags: ["User"],
+    }),
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
+    }),
+    getCustomers: build.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
+    }),
+    getTransactions: build.query({
+      query: ({  search }) => ({
+        url: "client/transactions",
+        method: "GET",
+        params: {search },
+      }),
+      providesTags: ["Transactions"],
+    }),
+    getGeography: build.query({
+      query: () => "client/geography",
+      providesTags: ["Geography"],
+    }),
+    getSales: build.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"],
+    }),
+  }),
+});
+
+export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery,useGetTransactionsQuery
+,useGetGeographyQuery,useGetSalesQuery
+} =
+  api;
